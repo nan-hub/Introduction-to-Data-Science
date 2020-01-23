a744466b606a44694485d7e79ebeadc66eb4e9ef
nan-hub <nl163@g.ucla.edu>
2020-01-23 23:11:11 +0000

    final version of runSim.R little format change use C function to combine two mses into a vector

--- hw1/runSim.R
+++ hw1/runSim.R
@@ -1,6 +1,6 @@
 ## parsing command arguments
 for (arg in commandArgs(TRUE)) {
-  eval(parse(text=arg))
+  eval(parse(text = arg))
 }
 
 ## check if a given integer is prime
@@ -23,23 +23,25 @@ estMeanPrimes = function (x) {
 
 #set seed for random number generation
 set.seed(seed)
-mseSampAvg=0
-msePrimeAvg=0
-for(r in 1:rep) {
+mseSampAvg = 0
+msePrimeAvg = 0
+for (r in 1:rep) {
   #parse the distribution information
-  if (dist == "gaussian"){
+  if (dist == "gaussian") {
     # simulate data
     x = rnorm(n)
-  }else if (dist == "t1"){
-    x=rt(n,df=1)
-  }else if (dist="t5"){
-    x=rt(n,df=5)
-  }else{
+  }else if (dist == "t1") {
+    x = rt(n, df = 1)
+  }else if (dist == "t5") {
+    x = rt(n, df = 5)
+  }else {
     stop("unrecognized distribution")
   }
   #try two methods
-  mseSampAvg=mseSampAvg+mean(x)^2
-  msePrimeAvg=msePrimeAvg+estMeanPrimes(x)^2
+  mseSampAvg = mseSampAvg + mean(x)^2
+  msePrimeAvg = msePrimeAvg + estMeanPrimes(x)^2
 }
-print(mseSampAvg/rep)
-print(msePrimeAvg/rep)
+MSE<-c(mseSampAvg / rep, msePrimeAvg / rep)
+MSE
+
+
